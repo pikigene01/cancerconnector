f60db599f1147a421955ab7e5c4a0ed2f5466445
pikigene <pikigene01@gmail.com>
2023-09-28 21:11:21 +0200

    update app

--- lib/pages/profilepage.dart
+++ lib/pages/profilepage.dart
@@ -1,8 +1,11 @@
 import 'package:cancerconnector/json/json_app.dart';
 import 'package:cancerconnector/themes/styles.dart';
 import 'package:cancerconnector/widgets/bottombar.dart';
+import 'package:cancerconnector/widgets/custom_btn.dart';
 import 'package:cancerconnector/widgets/topbar.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/my_text_fields.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -12,6 +15,8 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  var fullNameTextController = TextEditingController();
+  bool isDoctor = false;
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -23,33 +28,78 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget getBody({required size}) {
-    return SafeArea(
-        child: Column(
-      children: [
-        topBar(pageTitle: "Your Profile", size: size),
-        Image.asset(
-          doctors[1]["image"].toString(),
-          height: 70,
-        ),
-        const SizedBox(
-          height: 60,
-        ),
-        Text(
-          doctors[1]["name"],
-          style: const TextStyle(
-              fontSize: 20, fontWeight: FontWeight.w900, color: Colors.black),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/map-pin.png",
-              height: 30,
+    return SingleChildScrollView(
+      child: SafeArea(
+          child: Column(
+        children: [
+          topBar(pageTitle: "Your Profile", size: size),
+          Image.asset(
+            doctors[1]["image"].toString(),
+            height: 70,
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          Text(
+            doctors[1]["name"],
+            style: const TextStyle(
+                fontSize: 20, fontWeight: FontWeight.w900, color: Colors.black),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/map-pin.png",
+                height: 30,
+              ),
+              Text(doctors[1]!["location"].toString()),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyTextFieldWidget(
+              controller: fullNameTextController,
+              hintText: "Please Enter Your Full Name",
+              obscureText: false,
+              isBigInput: false,
             ),
-            Text(doctors[1]!["location"].toString()),
-          ],
-        ),
-      ],
-    ));
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Text("Are you a doctor?"),
+                Checkbox(
+                    value: isDoctor,
+                    onChanged: (data) {
+                      setState(() {
+                        isDoctor = !isDoctor;
+                      });
+                    }),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyTextFieldWidget(
+              controller: fullNameTextController,
+              hintText: "Please Enter Your Location",
+              obscureText: false,
+              isBigInput: false,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyTextFieldWidget(
+              controller: fullNameTextController,
+              hintText: "Please Enter Your Bio",
+              obscureText: false,
+              isBigInput: true,
+            ),
+          ),
+          MyCustomBtn(onTap: () {}, buttonText: "Update Profile"),
+        ],
+      )),
+    );
   }
 }
